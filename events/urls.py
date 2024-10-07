02f1564552fa877fe2eca26c6f275b8ed707e170
md5502 <m.baniasadi.d@gmail.com>
2024-10-07 21:37:44 +0330

    create the event list and detail views and corresponding urls and templates

--- events/urls.py
+++ events/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import events_list
+from .views import events_list, event_detail
 
-app_name = "evetns"
+app_name = "events"
 
 urlpatterns = [
     path("", events_list, name="events_list"),
+    path("<str:slug>/", event_detail, name="event_detail"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from .models import Event
+from .models import Event, EventImage
 
 
 def events_list(request):
     events = Event.objects.all()
+    context = {
+        "events": [],
+    }
+    for i in events:
+        first_image = EventImage.objects.filter(event=i).first().image
+        event = {
+            "title": i.title,
+            "date": i.schedule_date,
+            "image": first_image,
+            "slug": i.slug,
+        }
+        context["events"].append(event)
 
-    return render(request, "events/list.html", {"events":events})
+    return render(request, "events/events_list.html", context)
+
+
+def event_detail(request, slug):
+    event = get_object_or_404(Event, slug=slug)
+    event_images = EventImage.objects.filter(event=event)
+    extra_images = [i.image for i in event_images[1:]]
+
+    context = {"event": event, "main_image": event_images[0].image, "extra_images": extra_images}
+
+    return render(
+        request,
+        "events/event_detail.html",
+        context,
+    )
