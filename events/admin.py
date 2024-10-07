2f71d76f2664f1bf4da480fc07d3d21e148dbce7
md5502 <m.baniasadi.d@gmail.com>
2024-10-07 21:38:39 +0330

    register the event image model to the admin

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,7 @@
+import uuid
+
 from django.contrib import admin
+from django.utils.text import slugify
 
 from .models import Event, EventImage
 
@@ -10,6 +13,17 @@ class EventImageInline(admin.TabularInline):
 
 class EventAdmin(admin.ModelAdmin):
     inlines = [EventImageInline]
+    exclude = ("slug",)
+
+    def save_model(self, request, obj, form, change):
+        # Generate slug only if it's not provided
+        if not obj.slug:
+            obj.slug = slugify(
+                f"{str(uuid.uuid4()).split('-')[0]} {obj.title}",
+                allow_unicode=True,
+            )
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Event, EventAdmin)
+admin.site.register(EventImage)
