1e7262e956ed953bfbb72b20fd7be5f6461bdff8
md5502 <m.baniasadi.d@gmail.com>
2024-10-08 15:57:34 +0330

    add the views for the event detail and event register

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import events_list, event_detail
+from .views import event_detail, event_registration, events_list
 
 app_name = "events"
 
 urlpatterns = [
     path("", events_list, name="events_list"),
     path("<str:slug>/", event_detail, name="event_detail"),
+    path("register/<str:slug>/", event_registration, name="event_registration"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,9 +1,16 @@
 import jdatetime
+import markdown
+from django.contrib import messages
+from django.core.mail import EmailMessage
 from django.db.models import Q
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.template.loader import render_to_string
+from django.urls import reverse
 from django.utils.dateparse import parse_date
+from django.utils.translation import gettext_lazy as _
 
-from .models import Event, EventImage
+from .forms import EventUserRegistration
+from .models import Event, EventImage, EventUser
 
 
 def events_list(request):
@@ -30,18 +37,28 @@ def events_list(request):
             "title": i.title,
             "date": event_date,
             "image": first_image,
+            "location": i.location,  # Add location field
             "slug": i.slug,
         }
         context["events"].append(event)
 
     return render(request, "events/events_list.html", context)
 
+
 def event_detail(request, slug):
     event = get_object_or_404(Event, slug=slug)
     event_images = EventImage.objects.filter(event=event)
     extra_images = [i.image for i in event_images[1:]]
+    event_description_html = markdown.markdown(event.description)
 
-    context = {"event": event, "main_image": event_images[0].image, "extra_images": extra_images}
+    context = {
+        "event": event,
+        "main_image": event_images[0].image,
+        "extra_images": extra_images,
+        "event_date" : jdatetime.datetime.fromgregorian(date=event.schedule_date).strftime("%Y/%m/%d"),
+        "location": event.location,  # Add location field
+        "event_description_html": event_description_html,  # Add location field
+    }
 
     return render(
         request,
@@ -50,4 +67,62 @@ def event_detail(request, slug):
     )
 
 
+def event_registration(request, slug):
+    form = EventUserRegistration()
+    event = get_object_or_404(Event, slug=slug)
+    event_title = event.title
+    event_date = jdatetime.datetime.fromgregorian(date=event.schedule_date).strftime("%Y/%m/%d")
+    event_location = event.location  # Add location field
 
+    if request.method == "POST":
+        form = EventUserRegistration(request.POST)
+        if form.is_valid():
+
+            event_user = form.save(commit=False)
+            email = form.cleaned_data["email"]
+            already_user = EventUser.objects.filter(event=event, email=email)
+            if already_user:
+                messages.success(request, _("you already registered in this event"))
+                return redirect("events:event_detail", slug)
+
+            event_user.event = event
+            event_user.save()
+            name = form.cleaned_data["name"] + " " + form.cleaned_data["fname"]
+            send_registration_email(
+                user_email=email,
+                name=name,
+                event_title=event_title,
+                event_date=event_date,
+                event_location=event_location,  # Add location in the email
+                event_url=request.build_absolute_uri(reverse("events:event_detail", kwargs={"slug": event.slug})),
+            )
+
+            messages.success(request, _("you have been registered successfully"))
+            return redirect("events:events_list")
+    return render(
+        request, "events/event_registration.html",
+        {
+            "event_date": event_date,
+            "event_title": event_title,
+            "event_location": event_location,  # Pass location to the template
+            "form": form,
+        },
+    )
+
+
+def send_registration_email(user_email, name, event_title, event_date, event_location, event_url):
+    # Render the HTML email template
+    subject = "تأیید ثبت‌نام در رخداد"
+    message = render_to_string(
+        "emails/event_registration.html",
+        {
+            "first_name": name,
+            "event_title": event_title,
+            "event_date": event_date,
+            "event_location": event_location,  # Include location in the email
+            "event_url": event_url,
+        },
+    )
+    email = EmailMessage(subject, message, to=[user_email])
+    email.content_subtype = "html"  # Set the email to HTML format
+    email.send()
