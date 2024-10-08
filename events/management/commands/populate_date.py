02360f479515b85d351afeca1090b0f966b83924
md5502 <m.baniasadi.d@gmail.com>
2024-10-08 15:55:18 +0330

    update the command

--- events/management/commands/populate_date.py
+++ events/management/commands/populate_date.py
@@ -1,5 +1,4 @@
 from io import BytesIO
-
 import requests
 from django.core.files import File
 from django.core.management.base import BaseCommand
@@ -14,16 +13,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         fake = Faker()
-        number_of_events = 10  # Adjust the number of fake events
+        number_of_events = 10  # تعداد رویدادهای جعلی قابل تنظیم
 
         for _ in range(number_of_events):
-            # Create fake event data
+            # ایجاد داده‌های جعلی برای رویداد
             title = fake.sentence(nb_words=4)
-            description = fake.paragraph(nb_sentences=5)
+            description_paragraphs = fake.paragraphs(nb=5)  # تولید ۵ پاراگراف
+            description = "\n\n".join(description_paragraphs)  # اتصال پاراگراف‌ها با خط جدید
             slug = slugify(title, allow_unicode=True)
             schedule_date = fake.future_datetime()
 
-            # Create event instance
+            # ایجاد رویداد
             event = Event.objects.create(
                 title=title,
                 description=description,
@@ -31,15 +31,15 @@ class Command(BaseCommand):
                 schedule_date=schedule_date,
             )
 
-            # Download and attach random images to the event
-            image_url = "https://picsum.photos/800/600"  # Using Lorem Picsum for random images
+            # دانلود و ضمیمه کردن تصاویر تصادفی به رویداد
+            image_url = "https://picsum.photos/800/600"  # استفاده از Lorem Picsum برای تصاویر تصادفی
             image_content = requests.get(image_url).content
 
-            # Create an in-memory file for the image
+            # ایجاد فایل در حافظه برای تصویر
             image_io = BytesIO(image_content)
             image_name = f"{slug}.jpg"
 
-            # Save the image to the database
+            # ذخیره تصویر در دیتابیس
             event_image = EventImage(event=event)
             event_image.image.save(image_name, File(image_io), save=True)
 
