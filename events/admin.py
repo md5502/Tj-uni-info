66bb9243d497570e6b3dfbdcb95179e8a69ac726
md5502 <m.baniasadi.d@gmail.com>
2024-10-09 13:15:15 +0330

    add a option to download the registered user as a event and set verbose name for the app

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,11 @@
 import uuid
 
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.shortcuts import redirect
+from django.urls import path
 from django.utils.text import slugify
+
+from events.utils import generate_csv_response
 
 from .models import Event, EventImage, EventUser
 
@@ -15,10 +19,18 @@ class EventAdmin(admin.ModelAdmin):
     inlines = [EventImageInline]
     exclude = ("slug",)
 
+    actions = ["registered_users"]
+
     # Display fields in list view
     list_display = ["title", "location", "schedule_date", "created_at"]
     list_filter = ["location", "schedule_date"]
     search_fields = ["title", "location"]
+
+    @admin.action(description="دانلود لیست افرادی که در این رخداد ثبت‌نام کرده اند")
+    def registered_users(self, request, queryset):
+        # Get event IDs and pass them as a parameter in the redirect
+        event_ids = queryset.values_list("id", flat=True)
+        return redirect(f"download-registered-users/{','.join(map(str, event_ids))}")
 
     def save_model(self, request, obj, form, change):
         # Generate slug only if it's not provided
@@ -28,6 +40,25 @@ class EventAdmin(admin.ModelAdmin):
                 allow_unicode=True,
             )
         super().save_model(request, obj, form, change)
+
+    # Define custom admin URL
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                "download-registered-users/<str:event_ids>/",
+                self.admin_site.admin_view(self.download_registered_users),
+                name="download-registered-users",
+            ),
+        ]
+        return custom_urls + urls
+
+    # Custom view to handle CSV download
+    def download_registered_users(self, request, event_ids):
+        event_ids = event_ids.split(",")
+        events = self.model.objects.filter(id__in=event_ids)
+        messages.success(request, "لیست در حال دانلود است")
+        return generate_csv_response(events)
 
 
 class EventImageAdmin(admin.ModelAdmin):
---
+++ events/utils.py
@@ -0,0 +1,41 @@
+import csv
+from io import StringIO
+
+from django.http import HttpResponse
+
+
+def generate_csv_response(queryset):
+    # Create a file-like object for CSV content
+    output = StringIO()
+
+    # Define field names for the CSV
+    fieldnames = [
+        "نام",
+        "نام خانوادگی",
+        "شماره تماس",
+        "ایمیل",
+    ]
+
+    writer = csv.DictWriter(output, fieldnames=fieldnames)
+    writer.writeheader()
+
+    # Loop through each event and add user data
+    for obj in queryset:
+        users = obj.registered_users.all()
+        if users.exists():
+            for user in users:
+                user_info = {
+                    "نام": user.name,
+                    "نام خانوادگی": user.fname,
+                    "شماره تماس": user.phone_number,
+                    "ایمیل": user.email,
+                }
+                combined_info = {**user_info}
+
+                writer.writerow(combined_info)
+
+    # Create an HTTP response with the CSV content
+    response = HttpResponse(output.getvalue(), content_type="text/csv")
+    response["Content-Disposition"] = 'attachment; filename="registered_users.csv"'
+
+    return response
